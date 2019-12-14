3b369fcd29c753756a369872b1bdcec9580b4632
tima-kh-97 <tima.kh.97@gmail.com>
2019-12-14 09:39:40 +0600

    Project Milestone 3

--- XUnitTestProject/UsersServiceTests.cs
+++ XUnitTestProject/UsersServiceTests.cs
@@ -26,37 +26,58 @@ namespace XUnitTestProject
         [Fact]
         public void GetDetails()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            userService.GetDetails(1);
         }
 
         [Fact]
         public void Create()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            var user = new User() { Name = "Test", Surname = "Test" };
+            userService.Create(user);
         }
 
         [Fact]
         public void VerifyForExistence()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            var Phone = "87012323080";
+            userService.VerifyForExistence(Phone);
         }
 
         [Fact]
         public void Edit()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            var user = new User() {Id = 1, Name = "Test", Surname = "Test" };
+            userService.Edit(user);
         }
 
         [Fact]
         public void Delete()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            userService.Delete(2);
         }
 
         [Fact]
         public void UserExists()
         {
+            var fakeRepository = Moq.Mock.Of<IUserRepository>();
+            var userService = new UsersService(fakeRepository);
 
+            userService.UserExists(2);
         }
 
         private object Mock<T>()
